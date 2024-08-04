8809c5b4604f2da6785f5c8653602291f38d038d
Angelo Reale <12191809+angeloreale@users.noreply.github.com>
2024-08-04 17:43:44 +0100

    ar(fix) workflow

--- forge.config.ts
+++ forge.config.ts
@@ -3,7 +3,6 @@ import { MakerSquirrel } from '@electron-forge/maker-squirrel';
 import { MakerZIP } from '@electron-forge/maker-zip';
 import { MakerDeb } from '@electron-forge/maker-deb';
 import { MakerRpm } from '@electron-forge/maker-rpm';
-import { makeUniversalApp } from '@electron/universal';
 import { VitePlugin } from '@electron-forge/plugin-vite';
 import { FusesPlugin } from '@electron-forge/plugin-fuses';
 import { FuseV1Options, FuseVersion } from '@electron/fuses';
